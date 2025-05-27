1c01b10aa6bee7f5b166c62e38550f0a7b9b522e
Ayuningtyas24 <adyahseptiani@gmail.com>
2025-05-27 11:40:34 +0700

    menambahkan fungsi utama

--- inheritance/inheritance.cpp
+++ inheritance/inheritance.cpp
@@ -39,3 +39,13 @@ public:
     string perkenalan(){
         return "Hello, nama saya " + nama + " dari sekolah" + "\n\n"}
 };
+
+int main()
+{
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+    ;
+}
